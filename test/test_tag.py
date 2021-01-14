f122e168d74d488c7fb8375a0109c26b2756a796
Fabrice Laporte <fabrice@yescapa.com>
2021-01-14 21:58:43 +0100

    chore: apply black

--- test/test_tag.py
+++ test/test_tag.py
@@ -9,10 +9,11 @@ DEMO_RSRC = os.path.join(
     os.path.realpath(os.path.dirname(__file__)), "rsrc", "demo.mp3"
 )
 
+
 class TaggerTest(unittest.TestCase):
     def setUp(self):
-        self.demo_file = os.path.join(tempfile.mkdtemp(prefix="replica_"), 'demo.mp3')
+        self.demo_file = os.path.join(tempfile.mkdtemp(prefix="replica_"), "demo.mp3")
         shutil.copyfile(DEMO_RSRC, self.demo_file)
-    
+
     def test_get_tags(self):
         self.assertEqual(True, True)
