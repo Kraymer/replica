69bc2c5d966416721f1fd68cbfb6442663d544ed
Fabrice Laporte <fabrice@yescapa.com>
2021-01-14 21:52:39 +0100

    feat: add test/ dir

---
+++ test/test_tag.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+import shutil
+import tempfile
+
+DEMO_RSRC = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)), "rsrc", "demo.mp3"
+)
+
+class TaggerTest(unittest.TestCase):
+    def setUp(self):
+        self.demo_file = os.path.join(tempfile.mkdtemp(prefix="replica_"), 'demo.mp3')
+        shutil.copyfile(DEMO_RSRC, self.demo_file)
+    
+    def test_get_tags(self):
+        self.assertEqual(True, True)
